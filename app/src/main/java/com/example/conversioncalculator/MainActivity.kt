d87b0e081a2c7edf1344ecad93f88717ab546691
YuenShanTsang <A00270323@mycambrian.ca>
2023-03-08 22:55:43 -0500

    Completed UI by using Linear Layout, created spinner and buttons

--- app/src/main/java/com/example/conversioncalculator/MainActivity.kt
+++ app/src/main/java/com/example/conversioncalculator/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.conversioncalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fromSpinner: Spinner = findViewById(R.id.unit_spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.unit_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            fromSpinner.adapter = adapter
+        }
+
+        fromSpinner.onItemSelectedListener = this
+
+
     }
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val selectedItem = parent.getItemAtPosition(pos) as String
+        // Do something with the selected item
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Do nothing
+    }
+
+
 }
