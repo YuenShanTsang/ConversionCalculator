4d124d3ae29cc70fe8727f3de3f2639340f8795d
YuenShanTsang <A00270323@mycambrian.ca>
2023-03-09 22:08:31 -0500

    Completed UI with adding style and comments

--- app/src/main/java/com/example/conversioncalculator/MainActivity.kt
+++ app/src/main/java/com/example/conversioncalculator/MainActivity.kt
@@ -8,30 +8,45 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-
+    val number = 1
+    val convertedValue = number * 0.62
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fromSpinner: Spinner = findViewById(R.id.unit_spinner)
+        //Spinner
+        val unitSpinner: Spinner = findViewById(R.id.unit_spinner)
 
+        //Spinner adapter
         ArrayAdapter.createFromResource(
             this,
             R.array.unit_array,
-            android.R.layout.simple_spinner_item
+            R.layout.spinner_style
         ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            fromSpinner.adapter = adapter
+            adapter.setDropDownViewResource(R.layout.dropdown_style)
+            unitSpinner.adapter = adapter
         }
 
-        fromSpinner.onItemSelectedListener = this
+        unitSpinner.onItemSelectedListener = this
+
+
 
 
     }
+
+    //Spinner
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
         val selectedItem = parent.getItemAtPosition(pos) as String
-        // Do something with the selected item
+        val inputNumber =
+        var convertedValue = 0.0
+            when(selectedItem){
+            "km to mi" -> convertedValue = ${number*0.62}mi"
+        }
+
+        val resultTextView = findViewById(R.id.outputValue_text_view)
+        resultTextView.text = convertedValue.toString()
+    }
     }
 
     override fun onNothingSelected(parent: AdapterView<*>) {
