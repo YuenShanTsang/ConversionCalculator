f63ea0c5102ef734412f6ee1d1a2dfcc80e20a6b
YuenShanTsang <A00270323@mycambrian.ca>
2023-03-10 06:55:30 -0500

    Completed functions with using view binding

--- app/src/main/java/com/example/conversioncalculator/MainActivity.kt
+++ app/src/main/java/com/example/conversioncalculator/MainActivity.kt
@@ -5,53 +5,69 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import com.example.conversioncalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-    val number = 1
-    val convertedValue = number * 0.62
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //Spinner
-        val unitSpinner: Spinner = findViewById(R.id.unit_spinner)
-
-        //Spinner adapter
+        // Spinner
         ArrayAdapter.createFromResource(
             this,
             R.array.unit_array,
             R.layout.spinner_style
         ).also { adapter ->
             adapter.setDropDownViewResource(R.layout.dropdown_style)
-            unitSpinner.adapter = adapter
+            binding.unitSpinner.adapter = adapter
         }
+        binding.unitSpinner.onItemSelectedListener = this
 
-        unitSpinner.onItemSelectedListener = this
-
-
-
-
+        // Convert button
+        binding.convertButton.setOnClickListener {
+            conversion()
+        }
+        // Clear button
+        binding.clearButton.setOnClickListener {
+            binding.inputEditText.text.clear()
+            binding.outputValueTextView.text = "0"
+        }
     }
 
     //Spinner
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
-        val selectedItem = parent.getItemAtPosition(pos) as String
-        val inputNumber =
-        var convertedValue = 0.0
-            when(selectedItem){
-            "km to mi" -> convertedValue = ${number*0.62}mi"
-        }
-
-        val resultTextView = findViewById(R.id.outputValue_text_view)
-        resultTextView.text = convertedValue.toString()
-    }
+        // Do nothing
     }
 
     override fun onNothingSelected(parent: AdapterView<*>) {
         // Do nothing
     }
 
+    //Conversion
+    private fun conversion() {
+        val selectedItem = binding.unitSpinner.selectedItem as String
+
+        val inputValue = binding.inputEditText.text.toString().toDoubleOrNull() ?: return
+
+        var convertedValue = 0.0
+        when (selectedItem) {
+            "km to mi" -> convertedValue = inputValue * 0.62
+            "mi to km" -> convertedValue = inputValue * 1.61
+            "cm to in" -> convertedValue = inputValue * 0.39
+            "in to cm" -> convertedValue = inputValue * 2.54
+            "kg to lb" -> convertedValue = inputValue * 2.2
+            "lb to kg" -> convertedValue = inputValue * 0.45
+            "g to oz" -> convertedValue = inputValue * 0.04
+            "oz to g" -> convertedValue = inputValue * 28.35
+            "l to cups" -> convertedValue = inputValue * 4.17
+            "cup to l" -> convertedValue = inputValue * 0.24
+        }
+        //Display
+        binding.outputValueTextView.text = String.format("%.2f", convertedValue)
+    }
 
 }
+
