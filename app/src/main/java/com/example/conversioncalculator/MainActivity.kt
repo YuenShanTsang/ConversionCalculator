3bf71be26da3000decf9af49e36ec8db02d49a77
YuenShanTsang <A00270323@mycambrian.ca>
2023-03-11 15:22:04 -0500

    Completed the app and added some comments

--- app/src/main/java/com/example/conversioncalculator/MainActivity.kt
+++ app/src/main/java/com/example/conversioncalculator/MainActivity.kt
@@ -5,54 +5,76 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.conversioncalculator.databinding.ActivityMainBinding
 
+// Add AdapterView.OnItemSelectedListener to response the spinner
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
+    // Init view binding
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Setup view binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Spinner
+        // Setup spinner
         ArrayAdapter.createFromResource(
             this,
+            //Spinner items
             R.array.unit_array,
+            // Changed spinner style
             R.layout.spinner_style
         ).also { adapter ->
+            // Changed spinner dropdown style
             adapter.setDropDownViewResource(R.layout.dropdown_style)
             binding.unitSpinner.adapter = adapter
         }
         binding.unitSpinner.onItemSelectedListener = this
 
-        // Convert button
+        // When clicking the Convert button, it performs the conversion function
         binding.convertButton.setOnClickListener {
             conversion()
         }
-        // Clear button
+        // When clicking the Convert button, it clears the input text and returns the output to 0
         binding.clearButton.setOnClickListener {
             binding.inputEditText.text.clear()
             binding.outputValueTextView.text = "0"
         }
     }
 
-    //Spinner
+    // Spinner functions
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
         // Do nothing
     }
 
     override fun onNothingSelected(parent: AdapterView<*>) {
-        // Do nothing
+        // No action needed if nothing is selected
     }
 
-    //Conversion
+    // Conversion function
     private fun conversion() {
-        val selectedItem = binding.unitSpinner.selectedItem as String
+        // Get the selected item from spinner
+        val selectedItem = binding.unitSpinner.selectedItem
 
-        val inputValue = binding.inputEditText.text.toString().toDoubleOrNull() ?: return
+        // Get the text from the inputEditText view
+        val inputText = binding.inputEditText.text.toString()
 
+        // Convert the value to double, if cannot convert(no input/not a number), then return null
+        val inputValue = inputText.toDoubleOrNull()
+
+        // If inputValue is null, display a message and exit the function
+        if (inputValue == null) {
+            Toast.makeText(this, "Please enter a valid value", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // The converted value should be a double
         var convertedValue = 0.0
+
+        // Convert the input value according to the selected item
         when (selectedItem) {
             "km to mi" -> convertedValue = inputValue * 0.62
             "mi to km" -> convertedValue = inputValue * 1.61
@@ -65,7 +87,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             "l to cups" -> convertedValue = inputValue * 4.17
             "cup to l" -> convertedValue = inputValue * 0.24
         }
-        //Display
+
+        // Display the converted value with 2 sig. fig.
         binding.outputValueTextView.text = String.format("%.2f", convertedValue)
     }
 
